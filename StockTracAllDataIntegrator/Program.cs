3431f9361c387a735adf05249582a110ab9b50e9
David Eaton <DavidE@stocktrac.com>
2024-03-28 19:01:16 -0400

    Install Serilog.AspNetCore, Serilog.Sinks.File packages Configure logging Ignore log files and directories

--- StockTracAllDataIntegrator/Program.cs
+++ StockTracAllDataIntegrator/Program.cs
@@ -1,3 +1,4 @@
+using Serilog;
 using StockTracAllDataIntegrator.Services;
 
 namespace StockTracAllDataIntegrator
@@ -5,36 +6,58 @@ namespace StockTracAllDataIntegrator
     public class Program
     {
         public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+        {            // Configure Serilog first
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs/StockTracAllDataIntegrator-.txt"),
+                              rollingInterval: RollingInterval.Day,
+                              outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+                .CreateLogger();
 
-            // Add services to the container.
-            builder.Services.AddLogging(configure => configure.AddConsole())
-                .Configure<LoggerFilterOptions>(options =>
-                options.MinLevel = LogLevel.Information);
-            builder.Services.AddRazorPages();
-            builder.Services.AddHttpClient<ITokenService, TokenService>();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            // Log.Logger should be set before creating the builder so that we can log from there
+            try
             {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                Log.Information("Starting web host");
+
+                var builder = WebApplication.CreateBuilder(args);
+
+                // Add services to the container with the new Logger
+                builder.Host.UseSerilog(); // This will use the Serilog settings you've configured above
+
+                // Add services to the container.
+                builder.Services.AddRazorPages();
+                builder.Services.AddHttpClient<ITokenService, TokenService>();
+
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                if (!app.Environment.IsDevelopment())
+                {
+                    app.UseExceptionHandler("/Error");
+                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                    app.UseHsts();
+                }
+
+                app.UseHttpsRedirection();
+                app.UseStaticFiles();
+
+                app.UseRouting();
+
+                app.UseAuthorization();
+
+                app.MapRazorPages();
+
+                app.Run();
             }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapRazorPages();
-
-            app.Run();
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush(); // Ensure to flush and stop logging on shutdown
+            }
         }
     }
 }
